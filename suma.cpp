d9dea41ad4da82af072fb6c0a34210273df94ed2
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-04-03 06:59:32 -0500

    funcion

--- suma.cpp
+++ suma.cpp
@@ -3,14 +3,14 @@
 using std::cout;
 using std::endl;
 
-int suma(n);
+int suma(int n);
 int main()
 {
 int n=100;
-int suma=suma(n);
-cout << "La suma es: " << suma << endl;
+int s=suma(n);
+cout << "La suma es: " << s << endl;
 }
-int suma(n)
+int suma(int n)
 {
 	return n*(n+1)/2;
 }
